c0261818182fdcddbd0bbdb149b447cf91139106
Luizz <luizzfranca@gmail.com>
2023-12-18 15:19:16 -0300

    screens: completed screen adverts.tsx

--- App.tsx
+++ App.tsx
@@ -6,18 +6,15 @@ import {
   Karla_400Regular,
 } from "@expo-google-fonts/karla";
 
-import { SignIn } from "./src/screens/SignIn";
 import { Text } from "react-native";
-import { SignUp } from "./src/screens/SignUp";
-import { Home } from "./src/screens/Home";
-import { Details } from "./src/screens/Details";
+import { Adverts } from "./src/screens/Adverts";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_700Bold, Karla_400Regular });
 
   return (
     <>
-      {fontsLoaded ? <Details /> : <Text>erro na fonte</Text>}
+      {fontsLoaded ? <Adverts /> : <Text>erro na fonte</Text>}
       <StatusBar style="auto" />
     </>
   );
--- src/screens/Adverts.tsx
+++ src/screens/Adverts.tsx
@@ -1,9 +1,45 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import {
+  FlatList,
+  Platform,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { PlusIcon } from "react-native-heroicons/outline";
+import { Card } from "../components/Card";
 
+const ios = Platform.OS == "ios";
 export function Adverts() {
+  const [cards, setCards] = useState([1, 2, 3, 4]);
   return (
-    <View>
-      <Text>Anuncios</Text>
+    <View className="flex-1 p-5">
+      <SafeAreaView className={ios ? "mt-16" : "mt-14 items-center"}>
+        <View className="flex-row items-center ">
+          <Text className="font-bold text-16">Meus anúncios</Text>
+          <TouchableOpacity className="left-28">
+            <PlusIcon size="30" color="black" />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+      <View className="mt-4 flex-row items-center justify-between mx-3">
+        <Text className="text-16">9 anúncios</Text>
+        <Text>Select</Text>
+      </View>
+
+      <FlatList
+        data={cards}
+        keyExtractor={(item) => item.toString()}
+        renderItem={({ item }) => (
+          <Card title="Tênis vermelho" value="R$ 60,00" />
+        )}
+        numColumns={2}
+        columnWrapperStyle={{
+          justifyContent: "space-between",
+        }}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 }
